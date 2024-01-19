9119d6ed8a828fcd869f66cbfdcae3fc1fdde706
CI Workflow <ci@github.datatrails.ai>
2024-01-19 18:51:51 +0000

    feat: updated to go-datatrails-common-api@v0.3.7

--- assets/v2/assets/merklelogentry.pb.validate.go
+++ assets/v2/assets/merklelogentry.pb.validate.go
@@ -141,6 +141,114 @@ var _ interface {
 	ErrorName() string
 } = MerkleLogCommitValidationError{}
 
+// Validate checks the field values on MerkleLogCommitMongoDB with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the first error encountered is returned, or nil if there are no violations.
+func (m *MerkleLogCommitMongoDB) Validate() error {
+	return m.validate(false)
+}
+
+// ValidateAll checks the field values on MerkleLogCommitMongoDB with the rules
+// defined in the proto definition for this message. If any rules are
+// violated, the result is a list of violation errors wrapped in
+// MerkleLogCommitMongoDBMultiError, or nil if none found.
+func (m *MerkleLogCommitMongoDB) ValidateAll() error {
+	return m.validate(true)
+}
+
+func (m *MerkleLogCommitMongoDB) validate(all bool) error {
+	if m == nil {
+		return nil
+	}
+
+	var errors []error
+
+	// no validation rules for Index
+
+	// no validation rules for LeafIndex
+
+	// no validation rules for Idtimestamp
+
+	if len(errors) > 0 {
+		return MerkleLogCommitMongoDBMultiError(errors)
+	}
+
+	return nil
+}
+
+// MerkleLogCommitMongoDBMultiError is an error wrapping multiple validation
+// errors returned by MerkleLogCommitMongoDB.ValidateAll() if the designated
+// constraints aren't met.
+type MerkleLogCommitMongoDBMultiError []error
+
+// Error returns a concatenation of all the error messages it wraps.
+func (m MerkleLogCommitMongoDBMultiError) Error() string {
+	var msgs []string
+	for _, err := range m {
+		msgs = append(msgs, err.Error())
+	}
+	return strings.Join(msgs, "; ")
+}
+
+// AllErrors returns a list of validation violation errors.
+func (m MerkleLogCommitMongoDBMultiError) AllErrors() []error { return m }
+
+// MerkleLogCommitMongoDBValidationError is the validation error returned by
+// MerkleLogCommitMongoDB.Validate if the designated constraints aren't met.
+type MerkleLogCommitMongoDBValidationError struct {
+	field  string
+	reason string
+	cause  error
+	key    bool
+}
+
+// Field function returns field value.
+func (e MerkleLogCommitMongoDBValidationError) Field() string { return e.field }
+
+// Reason function returns reason value.
+func (e MerkleLogCommitMongoDBValidationError) Reason() string { return e.reason }
+
+// Cause function returns cause value.
+func (e MerkleLogCommitMongoDBValidationError) Cause() error { return e.cause }
+
+// Key function returns key value.
+func (e MerkleLogCommitMongoDBValidationError) Key() bool { return e.key }
+
+// ErrorName returns error name.
+func (e MerkleLogCommitMongoDBValidationError) ErrorName() string {
+	return "MerkleLogCommitMongoDBValidationError"
+}
+
+// Error satisfies the builtin error interface
+func (e MerkleLogCommitMongoDBValidationError) Error() string {
+	cause := ""
+	if e.cause != nil {
+		cause = fmt.Sprintf(" | caused by: %v", e.cause)
+	}
+
+	key := ""
+	if e.key {
+		key = "key for "
+	}
+
+	return fmt.Sprintf(
+		"invalid %sMerkleLogCommitMongoDB.%s: %s%s",
+		key,
+		e.field,
+		e.reason,
+		cause)
+}
+
+var _ error = MerkleLogCommitMongoDBValidationError{}
+
+var _ interface {
+	Field() string
+	Reason() string
+	Key() bool
+	Cause() error
+	ErrorName() string
+} = MerkleLogCommitMongoDBValidationError{}
+
 // Validate checks the field values on MerkleLogCommitMessage with the rules
 // defined in the proto definition for this message. If any rules are
 // violated, the first error encountered is returned, or nil if there are no violations.
